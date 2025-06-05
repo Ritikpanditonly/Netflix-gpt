799c39c1c272c6c89b59dfece8480ceaf5947814
Ritikpanditonly <pandeyritik236@gmail.com>
2025-06-05 18:54:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className = "text-3xl font-bold text-green-800">
+      Namste  netflix - gpt 
+      </div>
+  );
+}
+export default App;
+
+
+
