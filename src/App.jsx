fdea6f10b402f8d815cb7b3ea34f3e2ba42b5c32
Ritikpanditonly <pandeyritik236@gmail.com>
2025-06-07 12:23:53 +0530

    logiin,firebase,validate ,

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import './App.css'
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className = "text-3xl font-bold text-green-800">
-      Namste  netflix - gpt 
-      </div>
-  );
+  return <Body />;
 }
-export default App;
-
-
-
